94e54d004b142def11579c1f7e71e37723af5e9a
Zach Perkins <zach.perkins@fusionmedstaff.com>
2021-12-27 22:38:33 -0600

    Throwing spaghetti at the wall

--- xlsxtoJSON-2.js
+++ xlsxtoJSON-2.js
@@ -1,14 +1,34 @@
-fs = require('fs');
-let XLSX = require('xlsx');
-const workbook = XLSX.readFile('test.xlsx');
-const sheet_name_list = workbook.SheetNames;
-let jsonPagesArray = [];
-sheet_name_list.forEach(function(sheet) {
-        const jsonPage = {
-            name: sheet,
-            content: JSON.parse(JSON.stringify(XLSX.utils.sheet_to_json(workbook.Sheets[sheet],{defval:""})))
-        };
-        jsonPagesArray.push(jsonPage);
-    });
-    //console.log(data);
-    fs.writeFile('./cys-app/src/components/LocalPosts/cys.json', jsonPagesArray[0].content, function (err) {});
+//First, lets fetch the xlsx-to-json package
+const xlsxToJson = require('xlsx-to-json');
+
+//Set up the callback function
+var callback = function(err, result) {
+    //Check for errors
+    if(err) {
+      // Oops something went wrong!
+      console.log('Error : ', err);
+    }
+    //If everything's alright go ahead
+    else { 
+      /*
+        If you don't want to save the json to a file, instead just use it directly.
+        You can use the result returned by the xlsxToJson() function, directly like as follows:
+                doSomething(result);
+      */
+      console.log('You are done!');
+    }
+  };
+
+//Set up options for xlsx-to-json:
+var options = {
+    //Set the path to input file
+    input: "test.xlsx", 
+    //Set the path for Output file, if you want the json result saved in a file directly
+    output: "outputFile.json",
+    //Optionally you can specify which spreadsheet to take the input data from, 
+    //if you do not provide the sheet name, it reads it from the first sheet by default.
+    sheet: "Form Responses 3"
+};
+
+//Finally, call the xlsxToJson() to generate the Json from excel data.
+xlsxToJson(options, callback);
